3155146e5ad95385583d0f47f1a682d172342be7
Jozef Porubcin <jporubci@nd.edu>
2023-04-04 20:51:45 -0400

    Client can look up servers

--- sPinClient.py
+++ sPinClient.py
@@ -3,4 +3,32 @@
 # John Sullivan (jsulli28), Jozef Porubcin (jporubci)
 # sPinClient.py
 
+import json
+import http.client
 
+CATALOG_SERVER = 'catalog.cse.nd.edu:9097'
+SERVER_TYPE = 'sPin'
+
+class Server:
+    def __init__(self, address, port, lastheardfrom):
+        self.address = address
+        self.port = port
+        self.lastheardfrom = lastheardfrom
+
+class sPinClient:
+    def __init__(self):
+        self.servers = []
+        
+        
+    def server_lookup(self):
+        http_conn = http.client.HTTPConnection(CATALOG_SERVER)
+        http_conn.request('GET', '/query.json')
+        catalog = json.loads(http_conn.getresponse().read())
+        
+        self.servers = [Server(server['address'], server['port'], server['lastheardfrom']) for server in catalog if 'type' in server and server['type'] == SERVER_TYPE and 'address' in server and 'port' in server and 'lastheardfrom' in server]
+        
+        self.servers.sort(reverse=True, key=lambda x: x.lastheardfrom)
+        
+        
+    def sPinGET(self, key):
+        pass
