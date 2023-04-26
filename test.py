5e6fb3a77d93d57d757abf405fba687850869f37
Jozef Porubcin <jporubci@nd.edu>
2023-04-26 16:37:54 -0400

    rename

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 # John Sullivan (jsulli28), Jozef Porubcin (jporubci)
-# TestClient.py
+# test.py
 
 from client import sPinClient
 
 client = sPinClient.sPinClient()
-client.sPinADD('s')
+client.sPinADD('hello.txt')
