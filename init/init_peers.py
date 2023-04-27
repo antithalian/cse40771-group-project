9e6369c8176ecd5596276c650362fb49ff585fd0
Jozef Porubcin <jporubci@nd.edu>
2023-04-27 15:14:14 -0400

    Updated peer setup scripts

--- init/init_peers.py
+++ init/init_peers.py
@@ -5,21 +5,33 @@
 
 import shutil
 import os
+import json
 
 # Initializes peered server directory structure
-def init_peers(peers_dir, num_peers):
+def init_peers(peers_dir, peer_name, num_peers):
     shutil.rmtree(peers_dir + '/', ignore_errors=True)
     
     os.mkdir(peers_dir + '/')
     os.chdir(peers_dir + '/')
     for i in range(num_peers):
-        os.mkdir(peers_dir + str(i) + '/')
-        os.chdir(peers_dir + str(i) + '/')
-        os.mkdir('pins/')
-        os.mkdir('cache/')
+        os.mkdir(peer_name + str(i) + '/')
+        os.chdir(peer_name + str(i) + '/')
+        os.mkdir('pinned_files/')
+        os.mkdir('cached_files/')
         os.mkdir('meta/')
         os.chdir('meta/')
         os.mkdir('name/')
+        os.chdir('name/')
+        fd = os.open('name.txt', os.O_CREAT | os.O_EXCL)
+        os.close(fd)
+        os.chdir('../')
         os.mkdir('checkpoints/')
+        os.chdir('checkpoints/')
+        f = open('deletes.ckpt', 'x')
+        print(json.dumps(list()), file=f)
+        f.close()
+        fd = os.open('deletes.log', os.O_CREAT | os.O_EXCL)
+        os.close(fd)
+        os.chdir('../')
         os.chdir('../')
         os.chdir('../')
--- init/make_all.py
+++ init/make_all.py
@@ -1,19 +1,23 @@
 #!/bin/usr/env python3
 
+# John Sullivan (jsulli28), Jozef Porubcin (jporubci)
+# make_all.py
+
 import sys
 import os
 import init_files, init_peers
 
 files_dir = 'files'
 peers_dir = 'peers'
+peer_name = 'peer'
 num_peers = 3
 
 if len(sys.argv) == 1:
     init_files.init_files(files_dir)
-    init_peers.init_peers(peers_dir, num_peers)
+    init_peers.init_peers(peers_dir, peer_name, num_peers)
 
 elif len(sys.argv) == 2 and sys.argv[1] == 'init_files':
     init_files.init_files(files_dir)
 
 elif len(sys.argv) == 2 and sys.argv[1] == 'init_peers':
-    init_files.init_files(peers_dir)
+    init_files.init_peers(peers_dir, peer_name, num_peers)
