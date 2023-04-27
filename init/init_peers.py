32e4d95be1861ce62a74466dabed034d7cf296ce
Jozef Porubcin <jporubci@nd.edu>
2023-04-27 15:44:56 -0400

    Moved peer names to directory name instead of contents of a file in a directory. Also removed checkpoints/ and moved contents of checkpoints/ to just meta/

--- init/init_peers.py
+++ init/init_peers.py
@@ -5,6 +5,7 @@
 
 import shutil
 import os
+import uuid
 import json
 
 # Initializes peered server directory structure
@@ -13,25 +14,18 @@ def init_peers(peers_dir, peer_name, num_peers):
     
     os.mkdir(peers_dir + '/')
     os.chdir(peers_dir + '/')
-    for i in range(num_peers):
-        os.mkdir(peer_name + str(i) + '/')
-        os.chdir(peer_name + str(i) + '/')
+    for _ in range(num_peers):
+        peer_name = str(uuid.uuid4())
+        os.mkdir(peer_name + '/')
+        os.chdir(peer_name + '/')
         os.mkdir('pinned_files/')
         os.mkdir('cached_files/')
         os.mkdir('meta/')
         os.chdir('meta/')
-        os.mkdir('name/')
-        os.chdir('name/')
-        fd = os.open('name.txt', os.O_CREAT | os.O_EXCL)
-        os.close(fd)
-        os.chdir('../')
-        os.mkdir('checkpoints/')
-        os.chdir('checkpoints/')
         f = open('deletes.ckpt', 'x')
-        print(json.dumps(list()), file=f)
+        print(json.dumps(list()), end='', file=f)
         f.close()
         fd = os.open('deletes.log', os.O_CREAT | os.O_EXCL)
         os.close(fd)
-        os.chdir('../')
         os.chdir('../')
         os.chdir('../')
