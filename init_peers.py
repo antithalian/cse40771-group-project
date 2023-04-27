ad8fdf01c9cd12c849743a21d06aa286fdd785bd
Jozef Porubcin <jporubci@nd.edu>
2023-04-26 22:15:34 -0400

    Script to initialize peered servers directory structure

---
+++ init_peers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# John Sullivan (jsulli28), Jozef Porubcin (jporubci)
+# init_peers.py
+
+import shutil
+import os
+
+# Initializes peered server directory structure atomically
+def init_peers(name, n):
+    shutil.rmtree('.' + name + '/', ignoreerrors=True)
+    
+    os.mkdir('.' + name + '/')
+    os.chdir('.' + name + '/')
+    for i in range(n):
+        os.mkdir(name + str(i) + '/')
+        os.chdir(name + str(i) + '/')
+        os.mkdir('pins/')
+        os.mkdir('cache/')
+        os.mkdir('meta/')
+        os.chdir('meta/')
+        os.mkdir('name/')
+        os.mkdir('checkpoints/')
+        os.chdir('../')
+        os.chdir('../')
+    os.chdir('../')
+    
+    # La Grande Finale
+    os.rename('.' + name + '/', name + '/')
