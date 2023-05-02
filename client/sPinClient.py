e4bbbe73047628c5f3dfef6cbb5e3876f098b843
John Sullivan <jpsullivan2019@gmail.com>
2023-05-02 13:08:15 -0400

    client and server to this point

--- client/sPinClient.py
+++ client/sPinClient.py
@@ -12,6 +12,7 @@
 # os: to get size of file for sending file as a message using TCP
 
 import http.client
+import requests # for multipart mainly, but using for all now
 import json
 import random
 import time
@@ -19,6 +20,8 @@ import socket
 import uuid
 import math
 import os
+import hashlib
+import sys
 
 
 # CATALOG_SERVER: address and port of name server
@@ -38,12 +41,36 @@ class sPinPeer:
         self.lastheardfrom = lastheardfrom
 
 class sPinClient:
-    def __init__(self):
+    def __init__(self, main):
+        self.main = main
         # TCP socket
         self.s = None
         self.addr = None
         self.port = None
         
+    def get_peers(self):
+        # Get catalog
+        http_conn = http.client.HTTPConnection(CATALOG_SERVER)
+        http_conn.request('GET', '/query.json')
+        
+        # Parse catalog
+        catalog = json.loads(http_conn.getresponse().read())
+        http_conn.close()
+
+        # get peers from catalog
+        now = time.time()
+        all_peers = [entry for entry in catalog if entry.get('type', '') == ENTRY_TYPE and (now - entry.get('lastheardfrom') < 60)]
+
+        duplicates = {}
+        for peer in all_peers:
+            if peer['uuid'] in duplicates:
+                duplicates[peer['uuid']].append(peer)
+            else:
+                duplicates[peer['uuid']] = [peer]
+
+        peers = [max(dupes, key=lambda k: k['lastheardfrom']) for dupes in duplicates.values()]
+
+        return peers
         
     # Connects self's socket to a live sPin peer
     def _lookup_peer(self):
@@ -84,52 +111,170 @@ class sPinClient:
     # Adds a file to the network
     def sPinADD(self, filepath):
         
-        self._lookup_peer()
-        if self.s == None:
-            return
-        
         peers = self.get_peers()
+        if not len(peers):
+            print('error: no peers found')
+            sys.exit(1)
+
+        # generate object id
+        uuid_component = str(uuid.uuid4())
+        hash_component = self.get_digest(filepath)
+        object_id = uuid_component + ':' + hash_component
         
         # figure out k
         k = math.ceil(len(peers) / K_DENOM)
 
         # figure out which peers to pin to
-        pin_to = random.choices(peers, k)
-        
-        try:
-            f = open(filepath, 'rb')
-        except:
-            print('Failed to open file: ' + filepath)
-            return
-        
-        # HTTP POST to all peers
-        async for peer in pin_to:
-            pass
+        pin_to = random.choices(peers, k=k)
 
-        #
-        http_conn = http.client.HTTPConnection(self.addr + ':' + str(self.port))
-        http_conn.request('POST', '/' + str(uuid.uuid4()), f)
-        f.close()
-        http_conn.close()
-        
+        #print(pin_to)
+
+        for peer in pin_to:
+            try:
+                with open(filepath, 'rb') as to_upload:
+                    multipart = {'data': to_upload}                                                                                                                                                                 
+                    resp = requests.post(f'''http://{peer['name']}:{peer['port']}/add/{object_id}''', files=multipart)
+                    print(resp.status_code)
+            except FileNotFoundError as file_err:
+                if self.main: print(f'error: could not open file {filepath}')
+                sys.exit(1)
+            except requests.RequestException as req_err:
+                if self.main: print(f'error: could not connect to peer')
+                sys.exit(1)
+
+        if self.main: print(object_id)
+        return object_id
         
     # Gets the file associated with the given key
-    def sPinGET(self, key):
+    def sPinGET(self, object_id, filepath):
         
-        self._lookup_peer()
-        if self.s == None:
-            return
-        
-        # HTTP GET to peer
-        pass
+        peers = self.get_peers()
+        if not len(peers):
+            if self.main: print('error: no peers found')
+            sys.exit(1)
+
+        # figure out a few to try
+        to_try = random.sample(peers, k=len(peers))
+
+        success = False
+
+        for peer in to_try:
+            print(peer)
+            try:
+                http_conn = http.client.HTTPConnection(peer['name'], peer['port']) # TODO: add timeout?
+                http_conn.request('GET', f'/get/{object_id}')
+                #resp = requests.get(f'''http://{peer['name']}:{peer['port']}/get/{object_id}''')
+                # TODO error check
+                response = http_conn.getresponse()
+                if response.status == 200:
+                    response_body = response.read()
+                    try:
+                        with open(filepath, 'wb') as to_save:
+                            to_save.write(response_body)
+                    except OSError as file_err:
+                        if self.main: print(f'error: could not open or write to file {filepath}')
+                        sys.exit(1)
+                    success = True
+                    break
+                http_conn.close()
+
+            except OSError as file_err:
+                if self.main: print(f'error: could not connect to peer, trying next option')
+                
+            except http.client.HTTPException as http_err:
+                if self.main: print(f'error: could not connect to peer, trying next option')
+
+        if self.main: print('successfully retrieved file')
+        return 0 if success else 1
         
         
     # Requests deletion of the file associated with the given key
-    def sPinDEL(self, key):
+    def sPinDEL(self, object_id):
         
-        self._lookup_peer()
-        if self.s == None:
-            return
+        peers = self.get_peers()
+        if not len(peers):
+            if self.main: print('error: no peers found')
+            sys.exit(1)
         
-        # HTTP POST to peer
-        pass
+        # figure out k
+        k = math.ceil(len(peers) / K_DENOM)
+
+        # figure out which peers to request del from
+        del_from = random.choices(peers, k=k)
+
+        for peer in del_from:
+            try:
+                http_conn = http.client.HTTPConnection(peer['name'], peer['port']) # TODO: add timeout?
+                http_conn.request('POST', f'/del/{object_id}')
+                # TODO error check
+                http_conn.close()
+            except http.client.HTTPException as http_err:
+                if self.main: print(f'error: could not connect to peer')
+                sys.exit(1)
+
+        if self.main: print('successfully requested file deletion')
+
+    # helper to get hexdigest of a file
+    def get_digest(self, filepath):
+
+        hash = hashlib.sha256()
+
+        try:
+            with open(filepath, 'rb') as to_hash:
+                while True:
+                    chunk = to_hash.read(hash.block_size)
+                    if not chunk:
+                        break
+                    hash.update(chunk)
+        except FileNotFoundError as file_err:
+            if self.main: print(f'error: could not open file {filepath}')
+            sys.exit(1)
+            
+        return hash.hexdigest()
+    
+# CLI options
+# ADD
+# program add file
+# GET
+# program get object_id dst
+# DEL
+# program del object_id
+if __name__ == '__main__':
+
+    client = sPinClient(True)
+
+    usage = f'''usage:
+        {sys.argv[0]} add <filename> - add contents of <filename> to system, returning object id
+        {sys.argv[0]} get <object id> <filename> - get data for <object id>, if present, saving to <filename>
+        {sys.argv[0]} del <object id> - make a deletion request for <object id>
+        {sys.argv[0]} help - display this message
+        '''
+
+    if len(sys.argv) not in (3, 4):
+        print(usage)
+    elif sys.argv[1].lower() == 'get' and len(sys.argv) != 4:
+        print(usage)
+    elif sys.argv[1].lower() == 'help':
+        print(usage)
+    else:
+
+        # get all arguments into a form we can use
+        op = sys.argv[1].lower()
+        if op == 'add':
+            filename = sys.argv[2]
+        else:
+            object_id = sys.argv[2]
+
+        if op == 'get':
+            filename = sys.argv[3]
+
+        # TODO: sanity check object ids
+
+        if op == 'add':
+            client.sPinADD(filename)
+        elif op == 'get':
+            sys.exit(client.sPinGET(object_id, filename))
+        else: # del
+            client.sPinDEL(object_id)
+
+        sys.exit(0)
--- server/sPinServer.py
+++ server/sPinServer.py
@@ -44,10 +44,13 @@ class sPinServer:
     PIN_TRANS_BASE = 'pins'
     DEL_TRANS_BASE = 'dels'
     NAME_BASE = 'name'
-    # TODO: add checkpoint/log (YAML?)
-    # needs to hold both deletions and $UUID-$HASH combos
+    # temp file extension
+    TEMP_EXTENSION = 'new'
+    CKPT_EXTENSION = 'ckpt'
+    LOG_EXTENSION = 'log'
 
-    MAX_DEL_SIZE = 10_000 # 102 chars, 10,000 records
+    MAX_PIN_LOG_SIZE = 100
+    MAX_DEL_LOG_SIZE = 5_000 # 102 chars, ~5000 records
     MAX_CACHE_SIZE = 10_000_000_000 # 10GB
 
     def __init__(self):
@@ -57,8 +60,7 @@ class sPinServer:
         
         # $UUID-$HASH table
         # UUID:HASH to HASH table
-        #TODO: checkpoint load
-        self.pins = {}
+        self.pins = self.load_pins()
         # TODO: reconcile pins? i.e. if we don't have a certain file stored in PIN_DIR, remove it from our pins?
 
         # cache table
@@ -71,20 +73,186 @@ class sPinServer:
         # just a list of UUID:HASHes - this makes dropping the back end easier when the size gets too big
         # TODO: do something more complex so lookups are faster?
         #TODO: checkpoint load
-        self.dels = []
+        self.dels = self.load_dels()
 
         # worldview table
         # records should look like: UUID:HASH -> [{node: xyz, lastheardfrom: 123}, ... ]
-        self.world = collections.defaultdict(list) # TODO: reinitialize this from saved table?
+        self.world = collections.defaultdict(list)
 
         # peers table - to be initialized at first run of retrieve
         # records should look like: node uuid -> {host: , port: , lastheardfrom (? MAYBE ?)}
-        self.peers = None
+        self.peers = {}
 
         # port - to be initialized when server initialized
         self.port = None
         # host - same as above
         self.host = None
+
+    # log a deletion
+    # compress if necessary
+    def log_del(self, object_id):
+
+        log_location = f'{self.META_DIR}/{self.DEL_TRANS_BASE}.{self.LOG_EXTENSION}'
+
+        if self.del_log_length > self.MAX_DEL_LOG_SIZE:
+            # get from half to the end
+            new_dels = self.dels[self.del_log_length // 2:]
+
+            new_log_location = f'{self.META_DIR}/{self.DEL_TRANS_BASE}.{self.LOG_EXTENSION}.{self.TEMP_EXTENSION}'
+
+            # open and write to new log
+            with open(new_log_location, 'w') as new_log:
+                new_log.writelines([f'{obj_id}\n' for obj_id in new_dels])
+                new_log.flush()
+                os.fsync(new_log.fileno())
+
+            # close old log
+            self.del_log.close()
+
+            # replace with new
+            os.replace(new_log_location, log_location)
+
+            # open again
+            self.del_log = open(log_location, 'a')
+
+            # set dels to new_dels
+            self.dels = new_dels
+            self.del_log_length = len(self.dels)
+
+            print('info: truncated deletes')
+
+        try:
+            self.del_log.write(object_id + '\n')
+            self.del_log.flush()
+            os.fsync(self.del_log.fileno())
+            self.del_log_length += 1
+            return True
+        except OSError:
+            print('error: could not add to deletion log')
+            return False
+        
+    # load deletions from disk
+    def load_dels(self):
+
+        log_location = f'{self.META_DIR}/{self.DEL_TRANS_BASE}.{self.LOG_EXTENSION}'
+
+        # attempt to load from log location into dels
+        try:
+            with open(log_location, 'r') as del_log:
+                dels = [line.strip() for line in del_log]
+        except OSError:
+            print('error: could not load old deletion log')
+            dels = []
+
+        # open log location for appends
+        self.del_log = open(log_location, 'a')
+        self.del_log_length = len(dels)
+
+        return dels
+    
+    # log a pin transaction
+    # compress checkpoint if needed
+    def log_pins(self, type, object_id):
+
+        log_location = f'{self.META_DIR}/{self.PIN_TRANS_BASE}.{self.LOG_EXTENSION}'
+        ckpt_location = f'{self.META_DIR}/{self.PIN_TRANS_BASE}.{self.CKPT_EXTENSION}'
+
+        if self.pin_log_length > self.MAX_PIN_LOG_SIZE:
+
+            # write out full pins to new ckpt
+            with open(f'{ckpt_location}.{self.TEMP_EXTENSION}', 'w') as new_ckpt:
+                to_write = json.dumps(self.pins)
+                new_ckpt.write(to_write)
+                new_ckpt.flush()
+                os.fsync(new_ckpt.fileno())
+
+            os.replace(f'{ckpt_location}.{self.TEMP_EXTENSION}', ckpt_location)
+
+            self.pin_log.truncate(0)
+            self.pin_log.seek(0)
+            self.pin_log_length = 0
+
+            print('info: compressed pin log to checkpoint')
+
+        if type == 'add':
+            log_line = f'ADD:{object_id}\n'
+        else:
+            log_line = f'DEL:{object_id}\n'
+
+        try:
+            self.pin_log.write(log_line)
+            self.pin_log.flush()
+            os.fsync(self.pin_log.fileno())
+            self.pin_log_length += 1
+            return True
+        except OSError:
+            print('error: could not add to pin log')
+            return False
+        
+    # load pins from checkpoint and log
+    def load_pins(self):
+
+        log_location = f'{self.META_DIR}/{self.PIN_TRANS_BASE}.{self.LOG_EXTENSION}'
+        ckpt_location = f'{self.META_DIR}/{self.PIN_TRANS_BASE}.{self.CKPT_EXTENSION}'
+
+        # attempt to load checkpoint
+        try:
+            ckpt = open(ckpt_location, 'r')
+            ckpt_present = True
+        except OSError:
+            with open(ckpt_location, 'w') as new_ckpt:
+                new_ckpt.write('{}\n')
+                new_ckpt.flush()
+                os.fsync(new_ckpt.fileno())
+            ckpt_present = False
+
+        # attempt to load log
+        try:
+            log = open(log_location, mode='r')
+            log_present = True
+        except OSError:
+            log_present = False
+
+        # check if ckpt newer
+        if (ckpt_present and log_present) and (os.stat(ckpt.fileno()).st_mtime_ns > os.stat(log.fileno()).st_mtime_ns):
+            ckpt_newer = True
+        else:
+            ckpt_newer = False
+
+        self.pin_log_length = 0
+
+        # if no ckpt, make an empty table
+        if not ckpt_present:
+            pins = {}
+        else:
+            pins = json.loads(ckpt.read())
+
+            # if checkpoint older than txn, replay
+            if not ckpt_newer and log_present:
+                for line in log:
+                    self.pin_log_length += 1
+
+                    line = line.strip()
+                    elements = line.split(':')
+
+                    if elements[0] == 'ADD':
+                        pins[f'{elements[1]}:{elements[2]}'] = elements[2]
+                    else: # DEL
+                        try:
+                            del pins[f'{elements[1]}:{elements[2]}']
+                        except KeyError:
+                            pass
+
+        # clean up
+        if ckpt_present:
+            ckpt.close()
+        if log_present:
+            log.close()
+
+        # open log for appending
+        self.pin_log = open(log_location, 'a')
+
+        return pins
 
     def get_name(self):
         # track whether we need to write out
@@ -146,6 +314,8 @@ class sPinServer:
             write.close()
             await write.wait_closed()"""
 
+            print('info: updating nameserver about self')
+
             if self.DBG: pprint.pprint(msg)
 
             # TODO: consider using the above async/await form of this... possibly unnecessary since it's a single, quick connection
@@ -162,6 +332,8 @@ class sPinServer:
         # keep going forever
         while True:
 
+            print('info: retrieving peers from nameserver')
+
             # async retrieval of the JSON
             async with aiohttp.ClientSession() as session:
                 async with session.get(f'http://{self.NAMESERVER_NAME}:{self.NAMESERVER_PORT}{self.NAMESERVER_URL}') as resp:
@@ -171,7 +343,8 @@ class sPinServer:
             # find our project in nameserver json response
             # TODO: fix the fact that this will currently find a crapload of stuff if there's been a lot of churn at the nameserver recently
             #self.peers = [entry for entry in nameserver_json if entry.get('type', '') == self.NAMESERVER_TYPE and entry.get('uuid') != self.ID]
-            all_peers = [entry for entry in nameserver_json if entry.get('type', '') == self.NAMESERVER_TYPE and entry.get('uuid') != self.name]
+            now = time.time()
+            all_peers = [entry for entry in nameserver_json if entry.get('type', '') == self.NAMESERVER_TYPE and entry.get('uuid') != self.name and (now - entry.get('lastheardfrom') < 300)]
 
             if self.DBG: pprint.pprint(all_peers)
 
@@ -182,31 +355,40 @@ class sPinServer:
                 else:
                     duplicates[peer['uuid']] = [peer]
 
-            self.peers = [max(dupes, key=lambda k: k['lastheardfrom']) for dupes in duplicates.values()]
+            dedup = [max(dupes, key=lambda k: k['lastheardfrom']) for dupes in duplicates.values()]
+            self.peers = {record['uuid'] : record for record in dedup}
                 
             if self.DBG:     
                 print('received following peers:')
                 pprint.pprint(self.peers)
 
             # broadcast to all peers
-            await self.broadcast(self.peers)
+            await self.broadcast(list(self.peers.values()))
 
             # wait the required amount of time
             await asyncio.sleep(self.NAMESERVER_WAIT)
 
     # maintain the various data structures and call cleanup functions as needed
     async def maintain(self):
+        
+        # hack to let things fill up before maintain starts running
+        await asyncio.sleep(self.NAMESERVER_WAIT + 5)
+
         # keep going forever
         while True:
 
+            print('info: maintenance running')
+
             # remove anything too old from worldview
             now = time.time()
-            new_world = {}
+            new_world = collections.defaultdict(list)
             for obj, known_pins in self.world.items():
                 non_stale = [pin for pin in known_pins if (now - pin['lastheardfrom'] < self.STALENESS)]
                 new_world[obj] = non_stale
 
             self.world = new_world
+
+            print('info: maintenance updated worldview')
 
             # calculate k
             k = math.ceil(len(self.peers) / self.K_DENOM)
@@ -215,32 +397,35 @@ class sPinServer:
             for obj, known_pins in self.world.items():
 
                 if self.pins.get(obj):
-                    count = len(known_pins) + 1 # + 1 since self has it
+                    count = len(known_pins)
                     pins = [curr['node'] for curr in known_pins]
                     pins.append(self.name)
                     not_pins = [node for node in self.peers.keys() if node not in pins]
 
                     if count > k:
                         to_drop = pin_funcs.drop_pin(self.name, pins)
-                        if to_drop:
+                        if to_drop and to_drop != self.name:
                             node = self.peers[to_drop]
-                            node = f'''{node['host']:{node['port']}}'''
-                            await self.notify_drop(node, obj)
+                            #pprint.pprint(self.peers)
+                            name = f'''{node['name']}:{node['port']}'''
+                            print(f'info: maintenance instructing {name} to drop {obj}')
+                            await self.notify_drop(name, obj)
 
                     if count < k:
                         to_add = pin_funcs.add_pin(self.name, pins, not_pins)
-                        if to_add:
+                        if to_add and to_add != self.name:
                             node = self.peers[to_add]
-                            node = f'''{node['host']:{node['port']}}'''
-                            await self.notify_pin(node, obj)
+                            #pprint.pprint(self.peers)
+                            name = f'''{node['name']}:{node['port']}'''
+                            print(f'info: maintenance instructing {name} to pin {obj}')
+                            await self.notify_pin(name, obj)
 
-            # reduce size of dels if too large
-            if len(self.dels) > self.MAX_DEL_SIZE:
-                # TODO: drop oldest, write out new
-                pass
+            print('info: maintenance verified pin counts')
 
             # delete oldest files in cache if too large
             # TODO: just gonna let this be for now... hopefullly it won't get too big
+            #if True:
+            #    print('info: maintenance cleaned excess cache')
 
             # wait the required amount of time
             await asyncio.sleep(self.MAINTAIN_INTERVAL)
@@ -253,17 +438,17 @@ class sPinServer:
 
         for peer in peers:
 
-            async with aiohttp.ClientSession() as session:
-                print(f'''posting information to http://{peer['name']}:{peer['port']}/info:''')
+            async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5)) as session:
+                print(f'''info: posting information to http://{peer['name']}:{peer['port']}/info:''')
 
                 if self.DBG: pprint.pprint(self.pins)
 
                 try:
                     await session.post(f'''http://{peer['name']}:{peer['port']}/info''', json=payload)
+                    print(f'''info: posting information to http://{peer['name']}:{peer['port']}/info succeeded''')
                 except:
-                    # TODO: don't ignore failed?
-                    pass # ignoring failed for now, it'll come back around
-
+                    # TODO: differentiate more?
+                    print(f'''info: posting information to http://{peer['name']}:{peer['port']}/info failed: peer likely unavailable''')
 
     # ADD operation
     async def add_handler(self, request):
@@ -271,20 +456,39 @@ class sPinServer:
 
         hash = identifier.split(':')[1]
 
-        if self.DBG: print(f'receiving new file from client with identifier of {identifier}')
+        print(f'receiving file from client with identifier of {identifier}')
 
-        # handle saving posted data
-        body = await request.read()
-        with open(f'{self.PIN_DIR}/{hash}', 'wb') as file:
-            file.write(body)
-            file.flush()
-            os.fsync(file.fileno)
+        write_success = False
+        recv_success = False
+
+        async for field in (await request.multipart()):
+
+            if field.name == 'data':
+                recv_success = True
+                with open(f'{self.PIN_DIR}/{hash}', 'wb') as file:
+                    while True:
+                        chunk = await field.read_chunk()
+                        if not chunk:
+                            break
+                        file.write(chunk)
+
+                    # ensure written out
+                    file.flush()
+                    os.fsync(file.fileno())
+                    write_success = True
+            else:
+                continue # skip if not data field
 
         # add to pins dict
-        # TODO: checkpoint
+        self.log_pins('ADD', identifier)
         self.pins[identifier] = hash
 
-        return web.Response()
+        if write_success and recv_success:
+            return web.Response()
+        elif not recv_success:
+            return web.Response(status=400)
+        else: # failed receive data
+            return web.Response(status=500)
 
     # INFO operation
     async def info_handler(self, request):
@@ -292,17 +496,18 @@ class sPinServer:
         recv_time = time.time()
         payload = await request.json()
 
-        if self.DBG: 
-            print(f'receiving information from peer:')
+        print(f'''info: receiving information from peer''')
+        if self.DBG:
+            print(f'full payload:')
             pprint.pprint(payload)
         
         for record in payload:
             
             if record['object'] in self.dels:
-                await self.notify_deletion(record['node'], record['object'])
+                address = f'''{self.peers[record['node']]['name']}:{self.peers[record['node']]['port']}'''
+                await self.notify_deletion(address, record['object'])
             else:
                 # add record to world
-                # TODO: log/ckpt
                 self.world[record['object']].append({'node': record['node'], 'lastheardfrom': recv_time})
 
         return web.Response()
@@ -315,23 +520,26 @@ class sPinServer:
         # kinda hacky but
         if request.body_exists:
             drop = True
+        else:
+            drop = False
 
-        if self.DBG: print(f'deleting file with identifier of {identifier}')
+        print(f'servicing deletion request for object id: {identifier}')
 
         # add to dels
         # TODO: CHECKPOINT
-        if not drop: self.dels.append(identifier)
+        if not drop and identifier not in self.dels: 
+            self.dels.append(identifier)
 
         # delete from pins and cache
         try:
-             # TODO: CHECKPOINT
+            self.log_pins('DEL', identifier)
             del self.pins[identifier]
-        except ValueError:
+        except KeyError:
             pass
         if not drop:
             try:
                 del self.cache[identifier]
-            except ValueError:
+            except KeyError:
                 pass
 
         # delete file if no other pins refer to it
@@ -348,9 +556,10 @@ class sPinServer:
                 pass
 
         # notify others if needed
-        if not drop and self.world.get(identifier):
-            for node in self.world[identifier]:
-                await self.notify_deletion(node['node'], identifier)
+        #if not drop and self.world.get(identifier):
+        #    for node in self.world[identifier]:
+        #        address = f'''{self.peers[node['node']]['name']}:{self.peers[node['node']]['port']}'''
+        #        await self.notify_deletion(address, identifier)
 
         return web.Response()
 
@@ -445,7 +654,7 @@ class sPinServer:
         # TODO: hacky... is there a way around this?
         self.port = site._server.sockets[0].getsockname()[1]
         self.host = socket.getfqdn()
-        print(f'{self.host}:{self.port}')
+        print(f'{self.name} @ {self.host}:{self.port}')
 
         # run other tasks
         await asyncio.gather(self.update_nameserver(), self.retrieve_peers(), self.maintain())
