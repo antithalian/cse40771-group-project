8dbf9e1c5d1ba7034d16989efd7493d0a2d1d3f3
John Sullivan <jpsullivan2019@gmail.com>
2023-05-03 15:48:12 -0400

    cleanup

--- init/make_all.py
+++ init/make_all.py
@@ -10,7 +10,7 @@ files_dir = 'files'
 peers_dir = 'peers'
 peer_name = 'peer'
 num_peers = 3
-file_size = 5 * (1024 ** 2)
+file_size = 10 * (1024 ** 2)
 
 if len(sys.argv) == 1:
     init_files.init_files(files_dir)
@@ -19,5 +19,11 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2 and sys.argv[1] == 'init_files':
     init_files.init_files(files_dir, file_size)
 
+elif len(sys.argv) == 3 and sys.argv[1] == 'init_files':
+    init_files.init_files(files_dir, int(sys.argv[2]))
+
 elif len(sys.argv) == 2 and sys.argv[1] == 'init_peers':
     init_peers.init_peers(peers_dir, peer_name, num_peers)
+
+elif len(sys.argv) == 3 and sys.argv[1] == 'init_files':
+    init_files.init_peers(peers_dir, peer_name, int(sys.argv[2]))
